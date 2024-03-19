eb3c268d2a87055eba11665a38b75a1bb2fd6b48
Victorodrigues <viictorhgoliveira@gmail.com>
2024-03-19 15:06:48 -0300

    feat: Adding method to list everything and creating a typing file

--- src/database/useGoalDatabase.ts
+++ src/database/useGoalDatabase.ts
@@ -1,26 +1,42 @@
 import { useSQLiteContext } from "expo-sqlite/next"
 
-export type GoalCreateDatabase = {
-    name: string
-    total: string
-}
-
 //HOOK
 export function useGoalRespository(){
     const database = useSQLiteContext()
     
     function create(goal: GoalCreateDatabase){
-        const statement = database.prepareSync(
-            "INSERT INTO goals (name, total) VALUES($name ,$total)"
-        )
+        try{
+            const statement = database.prepareSync(
+                "INSERT INTO goals (name, total) VALUES($name ,$total)"
+            )
+    
+            statement.executeAsync({
+                $name: goal.name,
+                $total: goal.total
+            })
+        }
+        catch(error){
+            throw error;
+        }
+        
+    }
 
-        statement.executeAsync({
-            $name: goal.name,
-            $total: goal.total
-        })
+    function all(){
+        try{
+            return database.getAllSync<GoalResponseDatabase>(`
+                SELECT g.id, g.name, g.total , COALESCE(SUM(t.amount), 0) AS current
+                FROM goals AS g
+                LEFT JOIN transactions t ON t.goal_id = g.id
+                GROUP BY g.id, g.name, g.total;
+            `)
+        }
+        catch(error){
+            throw error
+        }
     }
 
     return {
         create,
+        all,
     }
 }
---
+++ src/database/useGoalDatabase.types.d.ts
@@ -0,0 +1,11 @@
+type GoalCreateDatabase = {
+    name: string
+    total: number
+}
+
+type GoalResponseDatabase = {
+    id: string,
+    name: string,
+    total: number,
+    current: number
+}
